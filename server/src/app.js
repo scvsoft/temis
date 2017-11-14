79155a3aff3c0247b7d6fde9d979571571eb7ff4
Rodrigo Uroz <rodrigouroz@gmail.com>
2017-11-14 12:21:52 -0300

    Add extra fields for facebook profile

--- server/src/app.js
+++ server/src/app.js
@@ -1,120 +1,47 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import jwt from 'jsonwebtoken'
-import expressJwt from 'express-jwt'
-import passport from 'passport'
 import helmet from 'helmet'
-import passportConfig from './lib/facebook'
 import Users from './models/user'
+import authenticationRoutes, {
+  authenticate,
+  getCurrentUser
+} from './lib/authentication'
 
-export const createToken = auth =>
-  jwt.sign(
-    {
-      id: auth.id
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_EXPIRATION
-    }
-  )
-
-export const createApp = () => {
+export default () => {
   const app = express()
 
   app.use(helmet())
 
   app.use(bodyParser.json())
 
-  // authentication setup
+  app.use('/authentication', authenticationRoutes())
 
-  // setup configuration for facebook login
-  passportConfig()
+  app.use(authenticate())
+  app.use(getCurrentUser)
 
-  const sendToken = (req, res) => {
-    res.setHeader('x-auth-token', req.token)
-    res.status(200).send(req.auth)
-  }
-
-  const generateToken = (req, res, next) => {
-    req.token = createToken(req.auth)
-    next()
-  }
-
-  app.post(
-    '/auth/facebook',
-    passport.authenticate('facebook-token', { session: false }),
-    (req, res, next) => {
-      if (!req.user) {
-        res.status(401).send('User Not Authenticated')
-      } else {
-        // prepare token for API
-        req.auth = {
-          id: req.user.id
-        }
-        next()
-      }
-    },
-    generateToken,
-    sendToken
-  )
-
-  // token handling middleware
-  const authenticate = expressJwt({
-    secret: process.env.JWT_SECRET,
-    requestProperty: 'auth',
-    getToken: req => {
-      if (req.headers['x-auth-token']) {
-        return req.headers['x-auth-token']
-      }
-      return null
-    }
-  })
-
-  const getCurrentUser = (req, res, next) => {
-    const user = Users.get(req.auth.id)
-    if (user) {
-      req.user = user
-      next()
+  app.get('/users/:userId', (req, res) => {
+    if (req.params.userId !== req.user.id) {
+      res.status(401)
     } else {
-      next('unknown user')
+      res.status(200).json(req.user)
     }
-  }
-
-  const getOne = (req, res) => {
-    const { user } = req
-
-    delete user.facebookProvider
-
-    res.json(user)
-  }
-
-  const errorHandler = (err, req, res, next) => {
-    // DEBUG && console.error("Error: ", err.name, err.message, err.status, err.stack)
-
-    // JWT
-    if (err.name === 'UnauthorizedError') {
-      res.status(401).send('invalid jwt token...')
-    }
-
-    // Facebook
-    if (err.name === 'InternalOAuthError') {
-      res.status(401).send('invalid facebook token...')
-    }
-
-    next()
-  }
-
-  app.get('/auth/me', authenticate, getCurrentUser, getOne)
-
-  app.get('/reports', authenticate, (req, res) => {
-    res.json([])
   })
 
-  app.put('/reports', authenticate, (req, res) => {
-    res.status(201).end()
+  app.put('/users/:userId', (req, res) => {
+    if (req.params.userId !== req.user.id) {
+      res.status(401)
+    } else {
+      const user = {
+        id: req.user.id,
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        birthday: req.body.birthday
+      }
+      Users.put(req.user.id, user)
+      res.status(200).json(user)
+    }
   })
-
-  app.use(errorHandler)
 
   return app
 }
--- server/test/app.test.js
+++ server/test/app.test.js
@@ -2,12 +2,12 @@ import nock from 'nock'
 import chai, { expect } from 'chai'
 import chaiHttp from 'chai-http'
 import http from 'http'
-import { createApp, createToken } from '../src/app'
+import dotenv from 'dotenv'
+import createApp from '../src/app'
+import { createToken } from '../src/lib/authentication'
+import Users from '../src/models/user'
 
-process.env.FACEBOOK_CLIENTID = '128732717834556'
-process.env.FACEBOOK_CLIENTSECRET = 'XXX'
-process.env.JWT_SECRET = 'f444WXmFIVxxbo3MvQndRGZ5'
-process.env.JWT_EXPIRATION = 60 * 120 // in seconds
+dotenv.config()
 
 const app = createApp()
 const server = http.createServer(app)
@@ -15,16 +15,105 @@ const server = http.createServer(app)
 chai.use(chaiHttp)
 
 describe('Server', () => {
-  describe('JWT Authentication', () => {
-    test('returns a valid response if JWT token is present and valid', done => {
-      const token = createToken({ id: 1001 })
+  beforeEach(() => {
+    nock('https://graph.facebook.com:443', { encodedQueryParams: true })
+      .get(/\/me$/)
+      .query(queryObj => queryObj.access_token === 'valid_token')
+      .reply(200, {
+        id: '106458953461566',
+        name: 'Margaret Wongberg',
+        email: 'margaret_zldursm_wongberg@tfbnw.net',
+        gender: 'female'
+      })
+
+    nock('https://graph.facebook.com:443', { encodedQueryParams: true })
+      .get(/\/me$/)
+      .query(queryObj => queryObj.access_token === 'invalid_token')
+      .reply(400, {
+        error: {
+          message: 'Invalid OAuth access token.',
+          type: 'OAuthException',
+          code: 190,
+          fbtrace_id: 'FdNcpt5NRHD'
+        }
+      })
+
+    Users.clear()
+
+    process.env.FACEBOOK_CLIENTID = '128732717834556'
+    process.env.FACEBOOK_CLIENTSECRET = 'XXX'
+    process.env.JWT_SECRET = 'f444WXmFIVxxbo3MvQndRGZ5'
+    process.env.JWT_EXPIRATION = '2h'
+  })
+
+  describe('Sign up', () => {
+    test('should return an error when token is invalid', done => {
+      chai
+        .request(server)
+        .post('/authentication/facebook')
+        .send({ access_token: 'invalid_token' })
+        .end((err, res) => {
+          expect(res).to.have.status(401)
+          done()
+        })
+    })
+
+    test('returns 201, the JWT token and the profile when the user is new', done => {
+      chai
+        .request(server)
+        .post('/authentication/facebook')
+        .send({ access_token: 'valid_token' })
+        .end((err, res) => {
+          expect(res).to.have.status(201)
+          expect(res.body).to.have.property('id', '106458953461566')
+          expect(res).to.have.header('x-auth-token')
+          done()
+        })
+    })
+  })
+
+  describe('Sign in', () => {
+    test('returns 200, the JWT token and the profile when the user is already registered', done => {
+      Users.put(106458953461566, {
+        id: '106458953461566',
+        name: 'Name',
+        email: 'Email',
+        gender: 'Gender',
+        birthday: 'Birthday'
+      })
 
       chai
         .request(server)
-        .get('/reports')
+        .post('/authentication/facebook')
+        .send({ access_token: 'valid_token' })
+        .end((err, res) => {
+          expect(res).to.have.status(200)
+          expect(res.body).to.have.property('id', '106458953461566')
+          expect(res).to.have.header('x-auth-token')
+          done()
+        })
+    })
+  })
+
+  describe('Get User profile', () => {
+    test('returns the profile if JWT token is present and valid', done => {
+      const token = createToken({ id: 106458953461566 })
+      Users.put(106458953461566, {
+        id: '106458953461566',
+        name: 'Name',
+        email: 'Email',
+        gender: 'Gender',
+        birthday: 'Birthday',
+        facebookProvider: {}
+      })
+
+      chai
+        .request(server)
+        .get('/users/106458953461566')
         .set('x-auth-token', token)
         .end((err, res) => {
           expect(res).to.have.status(200)
+          expect(res.body).to.have.property('id', '106458953461566')
           done()
         })
     })
@@ -32,7 +121,7 @@ describe('Server', () => {
     test('returns an authentication response error if JWT token is not present', done => {
       chai
         .request(server)
-        .get('/reports')
+        .get('/users/106458953461566')
         .end((err, res) => {
           expect(res).to.have.status(401)
           done()
@@ -45,7 +134,7 @@ describe('Server', () => {
 
       chai
         .request(server)
-        .get('/reports')
+        .get('/users/106458953461566')
         .set('x-auth-token', token)
         .end((err, res) => {
           expect(res).to.have.status(401)
@@ -54,51 +143,51 @@ describe('Server', () => {
     })
   })
 
-  describe('Facebook Authentication', () => {
-    // it('should return 401 status when not authenticated');
+  describe('Put User profile', () => {
+    test('sets the user profile if JWT token is present and valid', done => {
+      debugger
+      const token = createToken({ id: 106458953461566 })
+      const user = {
+        id: '106458953461566',
+        name: 'Name',
+        email: 'Email',
+        gender: 'Gender',
+        birthday: 'Birthday',
+        facebookProvider: {}
+      }
 
-    beforeAll(() => {
-      nock('https://graph.facebook.com:443', { encodedQueryParams: true })
-        .get(/\/me$/)
-        .query(queryObj => queryObj.access_token === 'valid_token')
-        .reply(200, {
-          id: '106458953461566',
-          name: 'Margaret Wongberg',
-          last_name: 'Wongberg',
-          first_name: 'Margaret',
-          email: 'margaret_zldursm_wongberg@tfbnw.net'
-        })
+      Users.put(106458953461566, user)
 
-      nock('https://graph.facebook.com:443', { encodedQueryParams: true })
-        .get(/\/me$/)
-        .query(queryObj => queryObj.access_token === 'invalid_token')
-        .reply(400, {
-          error: {
-            message: 'Invalid OAuth access token.',
-            type: 'OAuthException',
-            code: 190,
-            fbtrace_id: 'FdNcpt5NRHD'
-          }
-        })
-    })
-
-    test('should return a JWT token when facebook token is valid', done => {
       chai
         .request(server)
-        .post('/auth/facebook')
-        .send({ access_token: 'valid_token' })
+        .put('/users/106458953461566')
+        .send({ ...user, name: 'Olivia' })
+        .set('x-auth-token', token)
         .end((err, res) => {
           expect(res).to.have.status(200)
-          expect(res).to.have.header('x-auth-token')
+          expect(res.body).to.have.property('name', 'Olivia')
           done()
         })
     })
 
-    test('should return an error when token is invalid', done => {
+    test('returns an authentication response error if JWT token is not present', done => {
       chai
         .request(server)
-        .post('/auth/facebook')
-        .send({ access_token: 'invalid_token' })
+        .get('/users/106458953461566')
+        .end((err, res) => {
+          expect(res).to.have.status(401)
+          done()
+        })
+    })
+
+    test('returns an authentication response error if JWT token is expired', done => {
+      process.env.JWT_EXPIRATION = 0 // in seconds
+      const token = createToken({ id: 1001 })
+
+      chai
+        .request(server)
+        .get('/users/106458953461566')
+        .set('x-auth-token', token)
         .end((err, res) => {
           expect(res).to.have.status(401)
           done()
