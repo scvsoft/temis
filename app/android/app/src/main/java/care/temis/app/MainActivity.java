b7225b847398907ce754bf490711d9fe4572e650
JP Saraceno <jsarac@gmail.com>
2017-11-22 17:09:09 -0300

    add crashlytics on android

--- app/android/app/src/main/java/care/temis/app/MainActivity.java
+++ app/android/app/src/main/java/care/temis/app/MainActivity.java
@@ -1,1 +1,15 @@
+package care.temis.app;
 
+import android.os.Bundle;
+
+import com.reactnativenavigation.controllers.SplashActivity;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends SplashActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+    }
+}
