0a128b38f4e0b503cf19e1220bc4979b81da5855
Ezequiel Gonzalez Rial <gonrial@gmail.com>
2017-11-29 10:42:38 -0300

    AWS CodeDeploy first release

---
+++ scripts/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# update instance
+yum -y update
+
+# install general libraries like Java or ImageMagick
+#yum -y install default-jre ImageMagick
+
+# add nodejs to yum
+curl --silent --location https://rpm.nodesource.com/setup_9.x | bash -
+yum install -y nodejs
+
+# install pm2 module globaly
+npm install -g pm2
+pm2 update
---
+++ scripts/post_install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+cd ~/temis/server
+npm install
+npm run build
+
+# setup NODE_ENV
+if [ ! -z "$DEPLOYMENT_GROUP_NAME=" ]; then
+    export NODE_ENV=$DEPLOYMENT_GROUP_NAME
+
+    hasEnv=`grep "export NODE_ENV" ~/.bash_profile | cat`
+    if [ -z "$hasEnv" ]; then
+        echo "export NODE_ENV=$DEPLOYMENT_GROUP_NAME" >> ~/.bash_profile
+		echo "export FACEBOOK_CLIENTID=128732717834556" >> ~/.bash_profile
+		echo "export FACEBOOK_CLIENTSECRET=fada2088cc5fcf83788c16285d3a535c" >> ~/.bash_profile
+		echo "export JWT_SECRET=wGi9eZhag7yXBmeU7mM3c2QY" >> ~/.bash_profile
+		echo "export JWT_EXPIRATION=2h" >> ~/.bash_profile
+		echo "export PORT=3000" >> ~/.bash_profile
+		echo "export HOST=0.0.0.0" >> ~/.bash_profile
+		echo "export MONGODB_HOST=localhost:27017" >> ~/.bash_profile
+		echo "export MONGODB_DBNAME=temis" >> ~/.bash_profile
+    else
+        sed -i "/export NODE_ENV=\b/c\export NODE_ENV=$DEPLOYMENT_GROUP_NAME" ~/.bash_profile
+		sed -i "/export FACEBOOK_CLIENTID=\b/c\export FACEBOOK_CLIENTID=128732717834556" >> ~/.bash_profile
+		sed -i "/export FACEBOOK_CLIENTSECRET=\b/c\export FACEBOOK_CLIENTSECRET=fada2088cc5fcf83788c16285d3a535c" >> ~/.bash_profile
+		sed -i "/export JWT_SECRET=\b/c\export JWT_SECRET=wGi9eZhag7yXBmeU7mM3c2QY" >> ~/.bash_profile
+		sed -i "/export JWT_EXPIRATION=\b/c\export JWT_EXPIRATION=2h" >> ~/.bash_profile
+		sed -i "/export PORT=\b/c\export PORT=3000" >> ~/.bash_profile
+		sed -i "/export HOST=\b/c\export HOST=0.0.0.0" >> ~/.bash_profile
+		sed -i "/export MONGODB_HOST=\b/c\export MONGODB_HOST=localhost:27017" >> ~/.bash_profile
+		sed -i "/export MONGODB_DBNAME=\b/c\export MONGODB_DBNAME=temis" >> ~/.bash_profile
+    fi
+fi
+
+# add node to startup
+hasRc=`grep "su -l $USER" /etc/rc.d/rc.local | cat`
+if [ -z "$hasRc" ]; then
+    sudo sh -c "echo 'su -l $USER -c \"cd ~/temis/server;sh ./run.sh\"' >> /etc/rc.d/rc.local"
+fi
