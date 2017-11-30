f87981370f97935fa3bee7db9da1191061e3c360
Rodrigo Uroz <rodrigouroz@gmail.com>
2017-11-30 20:35:09 -0300

    Adds summary endpoint

--- server/src/routes/reports.js
+++ server/src/routes/reports.js
@@ -4,8 +4,9 @@ import reportsControllerBuilder from '../controllers/reports'
 export default () => {
   const router = express.Router()
 
-  const { get, post } = reportsControllerBuilder()
+  const { get, post, summary } = reportsControllerBuilder()
 
+  router.get('/summary', summary)
   router.get('/:reportId', get)
   router.post('/', post)
 
